d73eaad6a2dc17cec70f9cc0cbea3ae992eed522
Thirumalai-Tech-Developer <thirutechdeveloper@gmail.com>
2025-05-14 15:51:23 +0530

    init: base home page

--- gtking/settings.py
+++ gtking/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'admin_interface',
+    'colorfield',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members',
+    'base',
 ]
 
 MIDDLEWARE = [
--- gtking/urls.py
+++ gtking/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from base import views
 
 urlpatterns = [
-    path('', include('members.urls')),
+    path('', views.base, name="home"),
     path('admin/', admin.site.urls),
+    path('/', include('base.urls')),
 ]
