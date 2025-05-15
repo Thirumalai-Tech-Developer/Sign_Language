ac6605a69cd5df3c652f845186a77e99e8fc1753
Thirumalai-Tech-Developer <thirutechdeveloper@gmail.com>
2025-05-15 14:44:09 +0530

    feat: upoad video and save into local storage

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("/", views.base, name="index"),
     path('tosign/', views.tosign, name='tosign'),
     path('totext/', views.totext, name='totext'),
-
+    path('upload_video/', views.upload_video, name='upload_video'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import VideoFile
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import FileSystemStorage
 
 def base(request):
@@ -12,9 +13,12 @@ def tosign(request):
 def totext(request):
     return render(request, "totext.html")
 
+@csrf_exempt
 def upload_video(request):
-    if request.method == 'POST' and request.FILES['video_file']:
-        video_file = request.FILES['video_file']
-        fs = FileSystemStorage()
-        filename = fs.save(video_file.name, video_file)
+    if request.method == 'POST' and request.FILES.get('video'):
+        video = request.FILES['video']
+        fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+        filename = fs.save(video.name, video)
         file_url = fs.url(filename)
+        return JsonResponse({'url': file_url})
+    return JsonResponse({'error': 'No video uploaded'}, status=400)
